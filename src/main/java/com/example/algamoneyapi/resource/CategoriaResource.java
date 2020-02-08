667d5a3c16d6a7b332320b3dffb76b952ba174d4
leandrospricigo <leandrospricigo@gmail.com>
2020-02-07 22:13:41 -0300

    3_6_Cadastrando nova categoria

--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.example.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoneyapi.model.Categoria;
 import com.example.algamoneyapi.repository.CategoriaRepository;
@@ -19,6 +29,31 @@ public class CategoriaResource {
 	
 	@GetMapping
 	public List<Categoria> listar(){
-		return categoriaRepository.findAll();
+		 return categoriaRepository.findAll();
+		
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		//retorna http://localhost:8080/categorias/8 no header Location
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+		
+		
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+	return this.categoriaRepository.findById(codigo).orElse(null);
+	}
+	
+	
+	
+	
 }
